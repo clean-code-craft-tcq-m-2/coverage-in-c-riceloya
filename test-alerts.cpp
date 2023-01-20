e43684d0de9a553e5d29378de85cee3f8001a3d3
Loya Orduno Ricardo Ezequiel (BGSW/EMM4-MS) <Ricardo.LoyaOrduno@mx.bosch.com>
2023-01-20 09:01:25 -0600

    changes as per team feedback

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,8 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to Lower limits") {
-  REQUIRE(inferBreach(41, 43, 45) == TOO_HIGH);
+TEST_CASE("infers the breach according to all 3 level limits") {
+  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+  REQUIRE(inferBreach(47, 46, 50) == TOO_HIGH);
+  REQUIRE(inferBreach(40,43,44) == NORMAL);
 }
